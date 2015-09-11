e737c04032ffedf22d47517475fa4eee9f5a5d3a
leon-wee <leonwee27@gmail.com>
2015-09-11 13:45:40 +0700

    Done

--- lib/whats_the_number.rb
+++ lib/whats_the_number.rb
@@ -1,6 +1,12 @@
+require 'prime'
+
 class WhatsTheNumber
-  
+
   def self.sum_primes_less_than number
+    prime = Prime.instance
+    total = 0
+    prime.each(10000) { |x| total += x }
+    total
   end
 
 end
